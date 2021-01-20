8732de2b57d4e38b70f796471d081be1a8a92c8c
Dean <pleiades.95@mail.ru>
2021-01-20 21:33:17 +0600

    Add Day1 solution

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        for(int i = 1980; i < 2022; i+=4){
+            System.out.println("Олимпиада " + i + " года");
+        }
     }
 }
