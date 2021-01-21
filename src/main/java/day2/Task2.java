3671fa4fd782c575e5e8416b282d15a443aa83f9
Dean <pleiades.95@mail.ru>
2021-01-21 17:02:57 +0600

    complete day2 tasks

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,25 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scn = new Scanner(System.in);
+
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+
+        if(a >= b) {
+            System.out.println("Некорректный ввод");
+        }else{
+            for(int i = a; i < b; i++) {
+                a++;
+                if ((a < b) && (a % 5 == 0) && (a % 10 != 0)) {
+                    System.out.print(a + " ");
+
+                }
+            }
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        double x = scn.nextDouble();
+
+        if(x >= 5){
+            double y = ((x * x) - 10.0) / (x + 7.0);
+            System.out.println(y);
+        }else if(x > -3 && x < 5){
+            double y = ((x + 3.0)) * ((x * x) + 2.0 -4);
+            System.out.println(y);
+        }else{
+            double y = 420.0;
+            System.out.println(y);
+        }
 
     }
 }
