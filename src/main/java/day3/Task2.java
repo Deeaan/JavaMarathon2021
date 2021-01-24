1b9aa602deffeace421990b05ab78e4439f30350
Dean <pleiades.95@mail.ru>
2021-01-24 22:34:58 +0600

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scn = new Scanner(System.in);
+
+        while(true){
+
+            double num1 = scn.nextDouble();
+            double num2 = scn.nextDouble();
+
+            if(num2 == 0){
+                break;
+            }
+            double res = num1 / num2;
+            System.out.println(res);
+        }
     }
+
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,25 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        Scanner scn = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+
+            double num1 = scn.nextDouble();
+            double num2 = scn.nextDouble();
+
+            if(num2 == 0 || (num1 == 0 && num2 == 0)){
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            double res = num1 / num2;
+            System.out.println(res);
+
+        }
     }
 }
