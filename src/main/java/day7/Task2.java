9942fff9361173f5920f6f257c32c0ee5ded8a36
Dean <pleiades.95@mail.ru>
2021-01-30 17:37:47 +0600

    Complete Day8 tasks

--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -3,5 +3,6 @@ package day7;
 public class Task2 {
     public static void main(String[] args) {
 
+
     }
 }
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,36 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+
+//        long start = System.currentTimeMillis();
+//
+//        String str = new String(" ");
+//        for (int i = 0; i < 20001; i++){
+//            System.out.print(str + i);
+//        }
+//        //System.out.println(str + i);
+//
+//        System.out.println("\n");
+//        long finish = System.currentTimeMillis();
+//        long timeConsumedMillis = finish - start;
+//        System.out.println(timeConsumedMillis);
+
+
+
+        long start = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder(" ");
+        for (int i = 0; i < 20001; i++){
+            sb.append(i + " ");
+        }
+        System.out.print(sb.toString());
+
+        System.out.println("\n");
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
+
+
     }
+
 }
